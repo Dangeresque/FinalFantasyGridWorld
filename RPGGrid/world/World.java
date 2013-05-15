63eb8c6972716570b43ebbeb0bb5743d44837d40
Sean MacLane <dangeresquevi@gmail.com>
2013-05-14 19:16:06 -0700

    Window is fullscreen-ish

--- RPGGrid/world/World.java
+++ RPGGrid/world/World.java
@@ -74,6 +74,7 @@ public class World<T>
         if (frame == null)
         {
             frame = new WorldFrame<T>(this);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
             frame.setVisible(true);
         }
         else
